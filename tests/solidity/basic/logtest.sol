aed643249fee4fa6f85fd99ad165273cffb375c7
nickdammy <nickdammy2@gmail.com>
2022-11-07 01:17:45 -0600

    changes to the UI to resolve word wrap issue
    Pulled the latest version of Echidna.
    compiled, build and installed echidna on ubuntu.
    resolved word wrap issue.

---
+++ tests/solidity/basic/logtest.sol
@@ -0,0 +1,17 @@
+import "hardhat/console.sol";
+
+contract C { 
+  bytes32 name = "dami";
+  bool state;
+
+  function f(bytes32 st) public {
+    if (st[0] == "d" && st[1] == "a" && st[2] == "m"  && st[3] == "i" && st != name)
+        state = true;
+  }
+
+  function echidna_mutated() public returns (bool) {
+		console.log("%s:%s", "foo", "bar", "baz");
+    return !state;
+  }
+
+}
